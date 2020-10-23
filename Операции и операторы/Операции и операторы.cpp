c548a13b0ce41cd33938773c922d298579b19879
ivch-oss <iv.ch@icloud.com>
2020-10-23 12:27:18 +0300

    Добавьте файлы проекта.

---
+++ Операции и операторы/Операции и операторы.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Input your Name & Age\n";
+    string name = "???"; cin >> name;
+    int age = -1;
+    int a_m;
+    a_m = age * 12; cin >> a_m;
+    cout << "Hell0, " << name << "!" << "You age is of - " << a_m << "months!!!" << endl;
+    
+    return 0;
+}
+ 
